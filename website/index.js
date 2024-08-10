942a097e33afa325bb4f41d37001432e4914e17e
Sarang Ajay Patel <sarangp203@gmail.com>
2024-08-10 17:23:02 -0400

    api gateway

--- website/index.js
+++ website/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector("#visitor-counter");
 async function getCounter() {
-    let response = await fetch("https://lyei6ryberlcjwfdc2qtaesdoy0omjyr.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://c9mfnq9i17.execute-api.us-east-1.amazonaws.com/cloudResumeTest");
     let data = await response.json();
     counter.innerHTML = `Visitors: ${data}`;
 }
